42f120722f76f35caa1a0734670b9cd209de0c88
Karthik P <Karthik P@RESULTICKS-CHN->
2026-09-15 18:49:32 +0530

    Initial React learning project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Profile from "./components/Profile";
+import Header from "./components/Header";
+import Skills from "./components/Skills";
+
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Profile />
+            <Skills />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Skills.jsx
@@ -0,0 +1,15 @@
+function Skills() {
+    return (
+        <div>
+            <h2>Skills:</h2>
+            <ul>
+                <li>C#</li>
+                <li>.NET</li>
+                <li>SQL</li>
+                <li>React</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Skills;
